3f2eb737f1af9fa18630bc07c9c7d1f6236ce420
fafa-a <fabienpuech1@gmail.com>
2021-07-27 22:03:46 +0200

    feat(AppLink): added AppLink & extending-router-link-for-external-urls

---
+++ src/components/AppLink.vue
@@ -0,0 +1,25 @@
+<template>
+  <a
+    v-if="isExternal"
+    target="_blank"
+    rel="noopener"
+    class="external-link"
+    :href="to"
+    ><slot
+  /></a>
+  <router-link v-else v-bind="$props" class="internal-link"
+    ><slot
+  /></router-link>
+</template>
+
+<script setup>
+
+import { RouterLink } from "vue-router"
+defineProps({
+  ...RouterLink.props,
+})
+
+const isExternal = () => typeof to === "string" && to.startsWith("http")
+</script>
+
+<style lang="scss" scoped></style>

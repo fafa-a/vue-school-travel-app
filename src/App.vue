b1f0418b9025bc329cbba58cf1f244f74ea2eb8b
fafa-a <fabienpuech1@gmail.com>
2021-07-27 17:37:55 +0200

    feat(app & destinationShow): route-transitions

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
   <TheNavigation />
   <div class="container">
-    <RouterView :key="$route.path" />
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" :key="$route.path"></component>
+      </transition>
+    </router-view>
   </div>
 </template>
 
 <script setup>
 import TheNavigation from "@/components/TheNavigation.vue"
 </script>
+<style lang="css">
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.3s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+  transform: translateX(-30%);
+}
+</style>
--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -1,28 +1,30 @@
 <template>
-  <section class="destination">
-    <h1>{{ destination.name }}</h1>
-    <GoBack />
-    <div class="destination-details">
-      <img :src="`/images/${destination.image}`" :alt="destination.name" />
-      <p>{{ destination.description }}</p>
-    </div>
-  </section>
-  <section class="experiences">
-    <h2>Top experiences in {{ destination.name }}</h2>
-    <div class="cards">
-      <router-link
-        v-for="experience in destination.experiences"
-        :key="experience.slug"
-        :to="{
-          name: 'experience.show',
-          params: { experienceSlug: experience.slug },
-        }"
-      >
-        <ExperienceCard :experience="experience" />
-      </router-link>
-    </div>
-    <RouterView />
-  </section>
+  <div>
+    <section class="destination">
+      <h1>{{ destination.name }}</h1>
+      <GoBack />
+      <div class="destination-details">
+        <img :src="`/images/${destination.image}`" :alt="destination.name" />
+        <p>{{ destination.description }}</p>
+      </div>
+    </section>
+    <section class="experiences">
+      <h2>Top experiences in {{ destination.name }}</h2>
+      <div class="cards">
+        <router-link
+          v-for="experience in destination.experiences"
+          :key="experience.slug"
+          :to="{
+            name: 'experience.show',
+            params: { experienceSlug: experience.slug },
+          }"
+        >
+          <ExperienceCard :experience="experience" />
+        </router-link>
+      </div>
+      <RouterView />
+    </section>
+  </div>
 </template>
 
 <script setup>

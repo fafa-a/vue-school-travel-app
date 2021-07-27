4265e529d03b0f053aa932743af84033188eeca7
fafa-a <fabienpuech1@gmail.com>
2021-07-27 16:15:15 +0200

    feat(router): nested-routes with children attr in router

--- src/router/index.js
+++ src/router/index.js
@@ -13,12 +13,14 @@ const routes = [
     name: "destination.show",
     component: () => import("@/views/DestinationShow.vue"),
     props: (route) => ({ ...route.params, id: parseInt(route.params.id) }),
-  },
-  {
-    path: "/destination/:id/:slug/:experienceSlug",
-    name: "experience.show",
-    component: () => import("@/views/ExperienceShow.vue"),
-    props: (route) => ({ ...route.params, id: parseInt(route.params.id) }),
+    children: [
+      {
+        path: ":experienceSlug",
+        name: "experience.show",
+        component: () => import("@/views/ExperienceShow.vue"),
+        props: (route) => ({ ...route.params, id: parseInt(route.params.id) }),
+      },
+    ],
   },
 ]
 
